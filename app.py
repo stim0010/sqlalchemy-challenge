3874d023fd5029e369efbcabfb7585b290acea73
stim0010 <scott.stimpon@gmail.com>
2021-07-21 11:09:14 -0400

    added more queries and jsonify

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+@app.route("/api/v1.0/<start>` and `/api/v1.0/<start>/<end>")
+def 
